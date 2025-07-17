994c9099d4b9bc2ace3680c23ccc7993b75b13cb
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-07-17 08:26:01 +0300

    Automated commit - 2025-07-17 08:26:01

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -74,16 +74,3 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once()
 
 
-class TestHasLicense(unittest.TestCase):
-    """TestCase for GithubOrgClient.has_license"""
-
-    @parameterized.expand([
-        ({"license": {"key": "my_license"}}, "my_license", True),
-        ({"license": {"key": "other_license"}}, "my_license", False),
-    ])
-    def test_has_license(self, repo, license_key, expected):
-        """
-        Test the has_license static method with patched access_nested_map.
-        """
-        result = GithubOrgClient.has_license(repo, license_key)
-        self.assertEqual(result, expected)
