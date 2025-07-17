85b01dc04ca611341fcc57457e24e42eab3e2414
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-07-17 08:31:01 +0300

    no idea why this fails

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -73,16 +73,31 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_url.assert_called_once()
             mock_get_json.assert_called_once()
 
+# class TestHasLicense(unittest.TestCase):
+#     """TestCase for GithubOrgClient.has_license"""
+
+#     @parameterized.expand([
+#         ({"license": {"key": "my_license"}}, "my_license", True),
+#         ({"license": {"key": "other_license"}}, "my_license", False),
+#     ])
+#     def test_has_license(self, repo, license_key, expected):
+#         """
+#         Test the has_license static method with patched access_nested_map.
+#         """
+#         result = GithubOrgClient.has_license(repo, license_key)
+#         self.assertEqual(result, expected)
+        
 class TestHasLicense(unittest.TestCase):
-    """TestCase for GithubOrgClient.has_license"""
+    """Test case for GithubOrgClient.has_license"""
 
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
     ])
     def test_has_license(self, repo, license_key, expected):
-        """
-        Test the has_license static method with patched access_nested_map.
-        """
-        result = GithubOrgClient.has_license(repo, license_key)
-        self.assertEqual(result, expected)
+        """Test has_license method with real inputs"""
+        self.assertEqual(
+            GithubOrgClient.has_license(repo, license_key),
+            expected
+        )
+
