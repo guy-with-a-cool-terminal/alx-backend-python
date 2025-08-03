843c8e22f2c2995959f34b764026bff2f501073a
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-08-03 17:46:01 +0300

    signals and caching

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -7,12 +7,16 @@ def create_notification(sender,instance,created,**kwargs):
     if created:
         Notification.objects.create(user=instance.receiver,message=instance)
 
-@receiver(pre_save,sender=Message)
-def log_message_edit(sender,instance,**kwargs):
-    if instance.pk:
-        old_message = Message.objects.get(pk=instance.pk)
-        if old_message.content != instance.content:
-            MessageHistory.objects.create(message=old_message,old_content=old_message.content)
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:  # Check if the message already exists
+        old_message = Message.objects.filter(pk=instance.pk).first()  # Use filter to get the existing message
+        if old_message and old_message.content != instance.content:  # Check if the content has changed
+            MessageHistory.objects.create(message=old_message, old_content=old_message.content)
+            instance.edited = True  # Mark the message as edited
+            instance.edited_at = timezone.now()  # Set the edited timestamp
+            instance.edited_by = instance.sender  # Set the user who edited the message
+
 
 @receiver(post_delete,sender=User)
 def delete_user_related_data(sender,instance,**kwargs):
