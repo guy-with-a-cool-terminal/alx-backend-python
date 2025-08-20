76c7c975164e7173a03941e985ee0567f8ed05df
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-08-20 09:11:40 +0300

    add simple tests file to confirm pipeline works

--- messaging_app/tests.py
+++ messaging_app/tests.py
@@ -1,12 +1,13 @@
-import pytest
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_example(self):
-        """Simple test to verify pipeline works"""
-        assert 1 + 1 == 2
-        
-    def test_django_setup(self):
-        """Test Django is properly configured"""
-        from django.conf import settings
-        assert settings.DEBUG is not None
+def test_example():
+    """Simple test to verify pipeline works"""
+    assert 1 + 1 == 2
+    
+def test_string_operations():
+    """Test string operations"""
+    assert "hello".upper() == "HELLO"
+    
+def test_list_operations():
+    """Test list operations"""
+    my_list = [1, 2, 3]
+    assert len(my_list) == 3
+    assert 1 in my_list
